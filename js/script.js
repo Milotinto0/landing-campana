b0f4b6219844dc0bf8c6768727cabf4180d9d578
Milotinto <juancamilozea185@gmail.com>
2026-05-12 17:35:15 -0500

    feat: Agregar título principal y estructura de texto html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 
+function btn(){
+        const imgs = [
+        "./img/uribe.png",
+        "./img/uribe2.png",
+        "./img/uribe3.png"
+    ];
+    
+    let img = document.getElementById("img");
+
+        let aleatoria = Math.floor(Math.random()*3);
+        img.src = imgs[aleatoria];
+
+
+}
+
